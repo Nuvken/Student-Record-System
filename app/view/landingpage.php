14b041775544ac6bfa5004843d2edb599e7b7fc6
Kenny <rhayabusa960@gmai.com>
2025-10-19 19:28:58 +0800

    Landing Page

--- app/view/landingpage.php
+++ app/view/landingpage.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/project/public/css/landingpage.css">
     <title>Landing Page</title>
     <style>
         body, html {
@@ -11,17 +12,18 @@
     </style>
 </head>
 <body>
-    <header>
-        <a href="app/view/home.php" class="logo">KEN</a>
-        <nav>
+    <header class="main_header">
+        <a href="app/view/home.php" class="logo">K E N</a>
+        <nav class="nav_links">
             <a href="app/view/home.php">Home</a>
             <a href="app/view/about.php">About</a>
             <a href="app/view/features.php">Features</a>
             <a href="app/view/contact.php">Contact</a>
         </nav>
-        <button a href="app/view/login.php">Login</a><button>
-
-        <button a href="app/view/signup.php">Join Now</a><button>
+        <div class="btn">
+            <a href="app/view/login.php" class="login">Login</a>
+            <a href="app/view/signup.php" class="join">Join Now</a>
+        </div>
     </header>
     
 </body>
