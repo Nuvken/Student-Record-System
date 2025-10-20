4eefc77cfad77555b1e7a3047dfee28c1ab72bd9
Kenny <rhayabusa960@gmai.com>
2025-10-20 20:11:36 +0800

    changes is landing page

--- app/view/landingpage.php
+++ app/view/landingpage.php
@@ -4,27 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/project/public/css/landingpage.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
     <title>Landing Page</title>
-    <style>
-        body, html {
-            height: 100%;
-        }
-    </style>
 </head>
 <body>
-    <header class="main_header">
-        <a href="app/view/home.php" class="logo">K E N</a>
-        <nav class="nav_links">
-            <a href="app/view/home.php">Home</a>
-            <a href="app/view/about.php">About</a>
-            <a href="app/view/features.php">Features</a>
-            <a href="app/view/contact.php">Contact</a>
-        </nav>
-        <div class="btn">
-            <a href="app/view/login.php" class="login">Login</a>
-            <a href="app/view/signup.php" class="join">Join Now</a>
+    <div class="header">
+        <div class="logo">
+            <span>Student Management System</span>
         </div>
-    </header>
-    
+        <div class="nav-links">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+            <a href="">Pricing</a>
+        </div>
+        <div class="nav-actions">
+            <a href="" class="login">Login</a>
+            <a href="" class="join">Join Now</a>
+        </div>
+    </div>
 </body>
 </html>
